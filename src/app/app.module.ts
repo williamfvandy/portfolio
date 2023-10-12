f8d90a56fae5a462ce31c5a9a780d38b684f080f
Will Vanderburg <willvanderburg@wills-mbp.lan>
2023-10-12 16:22:57 -0400

    First Portfolio Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+    { path: 'projects', component: ProjectsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes), // Configure routes here
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
